73eb24f47fd441c2194117c88c7031fa66c85efc
Carlos E. Feria Vila <carlosthe19916@gmail.com>
2020-12-31 16:34:30 +0100

    Set eycloak by default (#17)
    * Disable healthcheck
    
    * set keycloak by default

--- src/test/java/io/github/project/openubl/xsender/basic/resources/config/BaseKeycloakTest.java
+++ src/test/java/io/github/project/openubl/xsender/basic/resources/config/BaseKeycloakTest.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package io.github.project.openubl.xsender.basic.resources.keycloak.config;
+package io.github.project.openubl.xsender.basic.resources.config;
 
 import io.restassured.RestAssured;
 
