0540516efd0577307ec629dabedd2f28bfebea39
Carlos Esteban Feria Vila <2582866+carlosthe19916@users.noreply.github.com>
2021-05-04 09:15:14 +0200

    Add companies delete test

--- src/test/java/io/github/project/openubl/xsender/resources/CompanyResourceTest.java
+++ src/test/java/io/github/project/openubl/xsender/resources/CompanyResourceTest.java
@@ -22,12 +22,23 @@ import java.util.UUID;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @QuarkusTest
 @QuarkusTestResource(KeycloakServer.class)
 @QuarkusTestResource(PostgreSQLServer.class)
 @QuarkusTestResource(KafkaServer.class)
 public class CompanyResourceTest extends BaseKeycloakTest {
+
+    final SunatCredentialsEntity credentials = SunatCredentialsEntity.Builder.aSunatCredentialsEntity()
+            .withSunatUsername("anyUsername")
+            .withSunatPassword("anyPassword")
+            .build();
+    final SunatUrlsEntity urls = SunatUrlsEntity.Builder.aSunatUrlsEntity()
+            .withSunatUrlFactura("https://e-factura.sunat.gob.pe/ol-ti-itcpfegem/billService?wsdl")
+            .withSunatUrlGuiaRemision("https://e-guiaremision.sunat.gob.pe/ol-ti-itemision-guia-gem/billService?wsdl")
+            .withSunatUrlPercepcionRetencion("https://e-factura.sunat.gob.pe/ol-ti-itemision-otroscpe-gem/billService?wsdl")
+            .build();
 
     NamespaceEntity namespace;
 
@@ -56,16 +67,6 @@ public class CompanyResourceTest extends BaseKeycloakTest {
     @Test
     public void getCompany() {
         // Given
-        SunatCredentialsEntity credentials = SunatCredentialsEntity.Builder.aSunatCredentialsEntity()
-                .withSunatUsername("anyUsername")
-                .withSunatPassword("anyPassword")
-                .build();
-        SunatUrlsEntity urls = SunatUrlsEntity.Builder.aSunatUrlsEntity()
-                .withSunatUrlFactura("https://e-factura.sunat.gob.pe/ol-ti-itcpfegem/billService?wsdl")
-                .withSunatUrlGuiaRemision("https://e-guiaremision.sunat.gob.pe/ol-ti-itemision-guia-gem/billService?wsdl")
-                .withSunatUrlPercepcionRetencion("https://e-factura.sunat.gob.pe/ol-ti-itemision-otroscpe-gem/billService?wsdl")
-                .build();
-
         CompanyEntity company = CompanyEntity.CompanyEntityBuilder.aCompanyEntity()
                 .withId(UUID.randomUUID().toString())
                 .withName("my company")
@@ -96,16 +97,6 @@ public class CompanyResourceTest extends BaseKeycloakTest {
     @Test
     public void getCompanyByNotOwner_shouldNotBeAllowed() {
         // Given
-        SunatCredentialsEntity credentials = SunatCredentialsEntity.Builder.aSunatCredentialsEntity()
-                .withSunatUsername("anyUsername")
-                .withSunatPassword("anyPassword")
-                .build();
-        SunatUrlsEntity urls = SunatUrlsEntity.Builder.aSunatUrlsEntity()
-                .withSunatUrlFactura("https://e-factura.sunat.gob.pe/ol-ti-itcpfegem/billService?wsdl")
-                .withSunatUrlGuiaRemision("https://e-guiaremision.sunat.gob.pe/ol-ti-itemision-guia-gem/billService?wsdl")
-                .withSunatUrlPercepcionRetencion("https://e-factura.sunat.gob.pe/ol-ti-itemision-otroscpe-gem/billService?wsdl")
-                .build();
-
         CompanyEntity company = CompanyEntity.CompanyEntityBuilder.aCompanyEntity()
                 .withId(UUID.randomUUID().toString())
                 .withName("my company")
@@ -134,6 +125,69 @@ public class CompanyResourceTest extends BaseKeycloakTest {
                 .get("/api/companies/" + company.getId())
                 .then()
                 .statusCode(200);
+        // Then
+    }
+
+    @Test
+    public void deleteCompany() {
+        // Given
+        CompanyEntity company = CompanyEntity.CompanyEntityBuilder.aCompanyEntity()
+                .withId(UUID.randomUUID().toString())
+                .withName("my company")
+                .withRuc("12345678910")
+                .withCreatedOn(new Date())
+                .withSunatCredentials(credentials)
+                .withSunatUrls(urls)
+                .withNamespace(namespace)
+                .build();
+
+        companyRepository.persist(company);
+
+        // When
+        given().auth().oauth2(getAccessToken("alice"))
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .when()
+                .delete("/api/companies/" + company.getId())
+                .then()
+                .statusCode(204);
+
+        // Then
+        assertNotNull(companyRepository.findById(company.getId()));
+    }
+
+    @Test
+    public void deleteCompany_byNotOwner() {
+        // Given
+        CompanyEntity company = CompanyEntity.CompanyEntityBuilder.aCompanyEntity()
+                .withId(UUID.randomUUID().toString())
+                .withName("my company")
+                .withRuc("12345678910")
+                .withCreatedOn(new Date())
+                .withSunatCredentials(credentials)
+                .withSunatUrls(urls)
+                .withNamespace(namespace)
+                .build();
+
+        companyRepository.persist(company);
+
+        // When
+        given().auth().oauth2(getAccessToken("admin"))
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .when()
+                .delete("/api/companies/" + company.getId())
+                .then()
+                .statusCode(404);
+
+        given().auth().oauth2(getAccessToken("alice"))
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .when()
+                .delete("/api/companies/" + company.getId())
+                .then()
+                .statusCode(204);
+
         // Then
     }
 
