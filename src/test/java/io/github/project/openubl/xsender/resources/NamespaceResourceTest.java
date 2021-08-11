a170f46338b4223d62ea65aaa5dae36fe2c915d8
Carlos Esteban Feria Vila <2582866+carlosthe19916@users.noreply.github.com>
2021-08-11 08:36:27 +0200

    add namespaces tests

--- src/test/java/io/github/project/openubl/xsender/resources/NamespaceResourceTest.java
+++ src/test/java/io/github/project/openubl/xsender/resources/NamespaceResourceTest.java
@@ -16,182 +16,177 @@
  */
 package io.github.project.openubl.xsender.resources;
 
+import io.github.project.openubl.xsender.ProfileManager;
 import io.github.project.openubl.xsender.idm.NamespaceRepresentation;
 import io.github.project.openubl.xsender.idm.NamespaceRepresentationBuilder;
-import io.github.project.openubl.xsender.resources.config.*;
-import io.quarkus.test.common.QuarkusTestResource;
+import io.github.project.openubl.xsender.resources.config.BaseKeycloakTest;
 import io.quarkus.test.common.http.TestHTTPEndpoint;
 import io.quarkus.test.junit.QuarkusTest;
+import io.quarkus.test.junit.TestProfile;
 import io.restassured.http.ContentType;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
-import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
 
-@Disabled
 @QuarkusTest
-//@QuarkusTestResource(KeycloakServer.class)
-//@QuarkusTestResource(MinioServer.class)
-//@QuarkusTestResource(ArtemisServer.class)
-//@QuarkusTestResource(PostgreSQLServer.class)
+@TestProfile(ProfileManager.class)
 @TestHTTPEndpoint(NamespaceResource.class)
 public class NamespaceResourceTest extends BaseKeycloakTest {
 
-//    @Test
-//    public void getNamespace() {
-//        // Given
-//        String nsId = "1";
-//
-//        // When
-//        given().auth().oauth2(getAccessToken("alice"))
-//                .contentType(ContentType.JSON)
-//                .when()
-//                .get("/" + nsId)
-//                .then()
-//                .statusCode(200)
-//                .body("name", is("my-namespace1"),
-//                        "description", is("description1")
-//                );
-//
-//        given().auth().oauth2(getAccessToken("alice"))
-//                .contentType(ContentType.JSON)
-//                .when()
-//                .get("/" + 10)
-//                .then()
-//                .statusCode(404);
-//        // Then
-//    }
-//
-//    @Test
-//    public void getNamespaceByNotOwner_shouldReturnNotFound() {
-//        // Given
-//        String nsId = "3";
-//
-//        // When
-//        given().auth().oauth2(getAccessToken("alice"))
-//                .contentType(ContentType.JSON)
-//                .when()
-//                .get("/" + nsId)
-//                .then()
-//                .statusCode(404);
-//
-//        given().auth().oauth2(getAccessToken("admin"))
-//                .header("Content-Type", "application/json")
-//                .when()
-//                .get("/" + nsId)
-//                .then()
-//                .statusCode(200);
-//        // Then
-//    }
-//
-//    @Test
-//    public void updateNamespace() {
-//        // Given
-//        String nsId = "1";
-//        NamespaceRepresentation newNamespace = NamespaceRepresentationBuilder.aNamespaceRepresentation()
-//                .withName("new name")
-//                .withDescription("my description")
-//                .build();
-//
-//        // When
-//        given().auth().oauth2(getAccessToken("alice"))
-//                .contentType(ContentType.JSON)
-//                .body(newNamespace)
-//                .when()
-//                .put("/" + nsId)
-//                .then()
-//                .statusCode(200)
-//                .body("id", is(nsId),
-//                        "name", is(newNamespace.getName()),
-//                        "description", is(newNamespace.getDescription())
-//                );
-//
-//        // Then
-//        given().auth().oauth2(getAccessToken("alice"))
-//                .contentType(ContentType.JSON)
-//                .when()
-//                .get("/" + nsId)
-//                .then()
-//                .statusCode(200)
-//                .body("id", is(nsId),
-//                        "name", is(newNamespace.getName()),
-//                        "description", is(newNamespace.getDescription())
-//                );
-//    }
-//
-//    @Test
-//    public void updateNamespaceByNotOwner_shouldNotBeAllowed() {
-//        // Given
-//        String nsId = "3";
-//        NamespaceRepresentation newNamespace = NamespaceRepresentationBuilder.aNamespaceRepresentation()
-//                .withName("new name")
-//                .withDescription("my description")
-//                .build();
-//
-//        // When
-//        given().auth().oauth2(getAccessToken("alice"))
-//                .contentType(ContentType.JSON)
-//                .body(newNamespace)
-//                .when()
-//                .put("/" + nsId)
-//                .then()
-//                .statusCode(404);
-//
-//        given().auth().oauth2(getAccessToken("admin"))
-//                .contentType(ContentType.JSON)
-//                .accept(ContentType.JSON)
-//                .body(newNamespace)
-//                .when()
-//                .put("/" + nsId)
-//                .then()
-//                .statusCode(200);
-//        // Then
-//    }
-//
-//    @Test
-//    public void deleteNamespace() {
-//        // Given
-//        String nsId = "1";
-//
-//        // When
-//        given().auth().oauth2(getAccessToken("alice"))
-//                .contentType(ContentType.JSON)
-//                .when()
-//                .delete("/" + nsId)
-//                .then()
-//                .statusCode(204);
-//
-//        // Then
-//        given().auth().oauth2(getAccessToken("alice"))
-//                .contentType(ContentType.JSON)
-//                .when()
-//                .get("/" + nsId)
-//                .then()
-//                .statusCode(404);
-//    }
-//
-//    @Test
-//    public void deleteNamespaceByNotOwner_shouldNotBeAllowed() {
-//        // Given
-//        String nsId = "3";
-//
-//        // When
-//        given().auth().oauth2(getAccessToken("alice"))
-//                .contentType(ContentType.JSON)
-//                .when()
-//                .delete("/" + nsId)
-//                .then()
-//                .statusCode(404);
-//
-//        given().auth().oauth2(getAccessToken("admin"))
-//                .contentType(ContentType.JSON)
-//                .when()
-//                .delete("/" + nsId)
-//                .then()
-//                .statusCode(204);
-//        // Then
-//    }
+    @Test
+    public void getNamespace() {
+        // Given
+        String nsId = "1";
+
+        // When
+        givenAuth("alice")
+                .contentType(ContentType.JSON)
+                .when()
+                .get("/" + nsId)
+                .then()
+                .statusCode(200)
+                .body("name", is("my-namespace1"),
+                        "description", is("description1")
+                );
+
+        givenAuth("alice")
+                .contentType(ContentType.JSON)
+                .when()
+                .get("/" + 10)
+                .then()
+                .statusCode(404);
+        // Then
+    }
+
+    @Test
+    public void getNamespaceByNotOwner_shouldReturnNotFound() {
+        // Given
+        String nsId = "3";
+
+        // When
+        givenAuth("alice")
+                .contentType(ContentType.JSON)
+                .when()
+                .get("/" + nsId)
+                .then()
+                .statusCode(404);
+
+        givenAuth("carlos")
+                .header("Content-Type", "application/json")
+                .when()
+                .get("/" + nsId)
+                .then()
+                .statusCode(200);
+        // Then
+    }
+
+    @Test
+    public void updateNamespace() {
+        // Given
+        String nsId = "1";
+        NamespaceRepresentation newNamespace = NamespaceRepresentationBuilder.aNamespaceRepresentation()
+                .withName("new name")
+                .withDescription("my description")
+                .build();
+
+        // When
+        givenAuth("alice")
+                .contentType(ContentType.JSON)
+                .body(newNamespace)
+                .when()
+                .put("/" + nsId)
+                .then()
+                .statusCode(200)
+                .body("id", is(nsId),
+                        "name", is(newNamespace.getName()),
+                        "description", is(newNamespace.getDescription())
+                );
+
+        // Then
+        givenAuth("alice")
+                .contentType(ContentType.JSON)
+                .when()
+                .get("/" + nsId)
+                .then()
+                .statusCode(200)
+                .body("id", is(nsId),
+                        "name", is(newNamespace.getName()),
+                        "description", is(newNamespace.getDescription())
+                );
+    }
+
+    @Test
+    public void updateNamespaceByNotOwner_shouldNotBeAllowed() {
+        // Given
+        String nsId = "3";
+        NamespaceRepresentation newNamespace = NamespaceRepresentationBuilder.aNamespaceRepresentation()
+                .withName("new name")
+                .withDescription("my description")
+                .build();
+
+        // When
+        givenAuth("alice")
+                .contentType(ContentType.JSON)
+                .body(newNamespace)
+                .when()
+                .put("/" + nsId)
+                .then()
+                .statusCode(404);
+
+        givenAuth("carlos")
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .body(newNamespace)
+                .when()
+                .put("/" + nsId)
+                .then()
+                .statusCode(200);
+        // Then
+    }
+
+    @Test
+    public void deleteNamespace() {
+        // Given
+        String nsId = "1";
+
+        // When
+        givenAuth("alice")
+                .contentType(ContentType.JSON)
+                .when()
+                .delete("/" + nsId)
+                .then()
+                .statusCode(204);
+
+        // Then
+        givenAuth("alice")
+                .contentType(ContentType.JSON)
+                .when()
+                .get("/" + nsId)
+                .then()
+                .statusCode(404);
+    }
+
+    @Test
+    public void deleteNamespaceByNotOwner_shouldNotBeAllowed() {
+        // Given
+        String nsId = "3";
+
+        // When
+        givenAuth("alice")
+                .contentType(ContentType.JSON)
+                .when()
+                .delete("/" + nsId)
+                .then()
+                .statusCode(404);
+
+        givenAuth("carlos")
+                .contentType(ContentType.JSON)
+                .when()
+                .delete("/" + nsId)
+                .then()
+                .statusCode(204);
+        // Then
+    }
 
 }
 
--- src/test/resources/db/io/github/project/openubl/xsender/resources/NamespaceResourceTest/V9999.0.1__namespaces.sql
+++ src/test/resources/db/io/github/project/openubl/xsender/resources/NamespaceResourceTest/V9999.0.1__namespaces.sql
@@ -1,7 +1,7 @@
 insert into namespace(id, name, description, owner, created_on, version)
 values ('1', 'my-namespace1', 'description1', 'alice', CURRENT_TIMESTAMP + INTERVAL '1 day', 1),
        ('2', 'my-namespace2', 'description2', 'alice', CURRENT_TIMESTAMP + INTERVAL '2 day', 1),
-       ('3', 'my-namespace3', 'description3', 'admin', CURRENT_TIMESTAMP + INTERVAL '3 day', 1);
+       ('3', 'my-namespace3', 'description3', 'carlos', CURRENT_TIMESTAMP + INTERVAL '3 day', 1);
 
 insert into company(id, ruc, name, created_on, namespace_id, version, sunat_username, sunat_password,
                     sunat_url_factura, sunat_url_guia_remision, sunat_url_percepcion_retencion)
