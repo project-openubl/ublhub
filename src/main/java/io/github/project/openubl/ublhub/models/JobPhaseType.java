0b5d8ca32ba909841b9173f2476ff0b47b7ac7bc
Carlos Esteban Feria Vila <2582866+carlosthe19916@users.noreply.github.com>
2022-01-25 18:30:22 +0100

    multi module

--- src/main/java/io/github/project/openubl/ublhub/models/JobPhaseType.java
+++
@@ -1,9 +0,0 @@
-package io.github.project.openubl.ublhub.models;
-
-public enum JobPhaseType {
-    FETCH_XML_FILE,
-    READ_XML_FILE,
-    SEND_XML_FILE,
-    SAVE_CDR,
-    VERIFY_TICKET,
-}
--- src/main/java/io/github/project/openubl/ublhub/models/JobRecoveryActionType.java
+++
@@ -1,6 +0,0 @@
-package io.github.project.openubl.ublhub.models;
-
-public enum JobRecoveryActionType {
-    RETRY_SEND,
-    RETRY_FETCH_CDR
-}
