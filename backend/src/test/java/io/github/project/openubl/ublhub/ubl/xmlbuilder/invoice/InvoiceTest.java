d8cf82653768151d91d8bff32cce51865db27cdc
Carlos Feria <2582866+carlosthe19916@users.noreply.github.com>
2022-02-12 10:18:14 +0100

    ICB test

--- backend/src/test/java/io/github/project/openubl/ublhub/ubl/xmlbuilder/invoice/InvoiceTest.java
+++ backend/src/test/java/io/github/project/openubl/ublhub/ubl/xmlbuilder/invoice/InvoiceTest.java
@@ -18,10 +18,7 @@ package io.github.project.openubl.ublhub.ubl.xmlbuilder.invoice;
 
 import io.github.project.openubl.ublhub.ProfileManager;
 import io.github.project.openubl.ublhub.ubl.XMLBuilder;
-import io.github.project.openubl.ublhub.ubl.content.models.common.Cliente;
-import io.github.project.openubl.ublhub.ubl.content.models.common.Contacto;
-import io.github.project.openubl.ublhub.ubl.content.models.common.Direccion;
-import io.github.project.openubl.ublhub.ubl.content.models.common.Proveedor;
+import io.github.project.openubl.ublhub.ubl.content.models.common.*;
 import io.github.project.openubl.ublhub.ubl.content.models.standard.general.BoletaFactura;
 import io.github.project.openubl.ublhub.ubl.content.models.standard.general.DocumentoDetalle;
 import io.github.project.openubl.xmlbuilderlib.models.catalogs.Catalog6;
@@ -184,4 +181,181 @@ public class InvoiceTest {
         assertSendSunat(xml);
     }
 
+    @Test
+    public void testInvoiceWithCustomProveedorDireccionAndContacto() throws Exception {
+        // Given
+        BoletaFactura input = new BoletaFactura();
+        input.serie = "F001";
+        input.numero = 1;
+
+        input.proveedor = new Proveedor();
+        input.proveedor.ruc = "12345678912";
+        input.proveedor.razonSocial = "Softgreen S.A.C.";
+        input.proveedor.contacto = new Contacto();
+        input.proveedor.contacto.email = "carlos@gmail.com";
+        input.proveedor.contacto.telefono = "+123456789";
+        input.proveedor.direccion = new Direccion();
+        input.proveedor.direccion.ubigeo = "050101";
+        input.proveedor.direccion.departamento = "Ayacucho";
+        input.proveedor.direccion.provincia = "Huamanga";
+        input.proveedor.direccion.distrito = "Jesus Nazareno";
+        input.proveedor.direccion.codigoLocal = "0101";
+        input.proveedor.direccion.urbanizacion = "000000";
+        input.proveedor.direccion.direccion = "Jr. Las piedras 123";
+        input.proveedor.direccion.codigoPais = "PE";
+
+        input.cliente = new Cliente();
+        input.cliente.nombre = "Carlos Feria";
+        input.cliente.numeroDocumentoIdentidad = "12121212121";
+        input.cliente.tipoDocumentoIdentidad = Catalog6.RUC.toString();
+
+        DocumentoDetalle detalle1 = new DocumentoDetalle();
+        detalle1.descripcion = "Item1";
+        detalle1.cantidad = new BigDecimal(10);
+        detalle1.precio = new BigDecimal(100);
+
+        DocumentoDetalle detalle2 = new DocumentoDetalle();
+        detalle2.descripcion = "Item2";
+        detalle2.cantidad = new BigDecimal(10);
+        detalle2.precio = new BigDecimal(100);
+
+        input.detalle = Arrays.asList(detalle1, detalle2);
+
+        // When
+        UniAssertSubscriber<String> subscriber = xmlBuilder.enrichAndRenderAsync(input).subscribe().withSubscriber(UniAssertSubscriber.create());
+
+        // Then
+        subscriber.assertCompleted();
+
+        String xml = subscriber.getItem();
+        assertSnapshot(xml, getClass(), "customProveedorDireccionAndContacto.xml");
+        assertSendSunat(xml);
+    }
+
+    @Test
+    public void testInvoiceWithCustomFirmante() throws Exception {
+        // Given
+        BoletaFactura input = new BoletaFactura();
+        input.serie = "F001";
+        input.numero = 1;
+
+        input.proveedor = new Proveedor();
+        input.proveedor.ruc = "12345678912";
+        input.proveedor.razonSocial = "Softgreen S.A.C.";
+
+        input.cliente = new Cliente();
+        input.cliente.nombre = "Carlos Feria";
+        input.cliente.numeroDocumentoIdentidad = "12121212121";
+        input.cliente.tipoDocumentoIdentidad = Catalog6.RUC.toString();
+
+        input.firmante = new Firmante();
+        input.firmante.ruc = "000000000000";
+        input.firmante.razonSocial = "Wolsnut4 S.A.C.";
+
+        DocumentoDetalle detalle1 = new DocumentoDetalle();
+        detalle1.descripcion = "Item1";
+        detalle1.cantidad = new BigDecimal(10);
+        detalle1.precio = new BigDecimal(100);
+
+        DocumentoDetalle detalle2 = new DocumentoDetalle();
+        detalle2.descripcion = "Item2";
+        detalle2.cantidad = new BigDecimal(10);
+        detalle2.precio = new BigDecimal(100);
+
+        input.detalle = Arrays.asList(detalle1, detalle2);
+
+        // When
+        UniAssertSubscriber<String> subscriber = xmlBuilder.enrichAndRenderAsync(input).subscribe().withSubscriber(UniAssertSubscriber.create());
+
+        // Then
+        subscriber.assertCompleted();
+
+        String xml = subscriber.getItem();
+        assertSnapshot(xml, getClass(), "customFirmante.xml");
+        assertSendSunat(xml);
+    }
+
+    @Test
+    public void testInvoiceWithICB_precioUnitario() throws Exception {
+        // Given
+        BoletaFactura input = new BoletaFactura();
+        input.serie = "F001";
+        input.numero = 1;
+
+        input.proveedor = new Proveedor();
+        input.proveedor.ruc = "12345678912";
+        input.proveedor.razonSocial = "Softgreen S.A.C.";
+
+        input.cliente = new Cliente();
+        input.cliente.nombre = "Carlos Feria";
+        input.cliente.numeroDocumentoIdentidad = "12121212121";
+        input.cliente.tipoDocumentoIdentidad = Catalog6.RUC.toString();
+
+        DocumentoDetalle detalle1 = new DocumentoDetalle();
+        detalle1.descripcion = "Item1";
+        detalle1.cantidad = new BigDecimal(10);
+        detalle1.precio = new BigDecimal(100);
+        detalle1.icbAplica = true;
+
+        DocumentoDetalle detalle2 = new DocumentoDetalle();
+        detalle2.descripcion = "Item2";
+        detalle2.cantidad = new BigDecimal(10);
+        detalle2.precio = new BigDecimal(100);
+        detalle2.icbAplica = true;
+
+        input.detalle = Arrays.asList(detalle1, detalle2);
+
+        // When
+        UniAssertSubscriber<String> subscriber = xmlBuilder.enrichAndRenderAsync(input).subscribe().withSubscriber(UniAssertSubscriber.create());
+
+        // Then
+        subscriber.assertCompleted();
+
+        String xml = subscriber.getItem();
+        assertSnapshot(xml, getClass(), "icb.xml");
+        assertSendSunat(xml);
+    }
+
+//    @Test
+//    public void testInvoiceWithICB_precioConIgv() throws Exception {
+//        // Given
+//        BoletaFactura input = new BoletaFactura();
+//        input.serie = "F001";
+//        input.numero = 1;
+//
+//        input.proveedor = new Proveedor();
+//        input.proveedor.ruc = "12345678912";
+//        input.proveedor.razonSocial = "Softgreen S.A.C.";
+//
+//        input.cliente = new Cliente();
+//        input.cliente.nombre = "Carlos Feria";
+//        input.cliente.numeroDocumentoIdentidad = "12121212121";
+//        input.cliente.tipoDocumentoIdentidad = Catalog6.RUC.toString();
+//
+//        DocumentoDetalle detalle1 = new DocumentoDetalle();
+//        detalle1.descripcion = "Item1";
+//        detalle1.cantidad = new BigDecimal(10);
+//        detalle1.precio = new BigDecimal(118);
+//        detalle1.precioConImpuestos = true;
+//        detalle1.icbAplica = true;
+//
+//        DocumentoDetalle detalle2 = new DocumentoDetalle();
+//        detalle2.descripcion = "Item2";
+//        detalle2.cantidad = new BigDecimal(10);
+//        detalle2.precio = new BigDecimal(118);
+//        detalle2.precioConImpuestos = true;
+//        detalle2.icbAplica = true;
+//
+//        input.detalle = Arrays.asList(detalle1, detalle2);
+//
+//        // When
+//        UniAssertSubscriber<String> subscriber = xmlBuilder.enrichAndRenderAsync(input).subscribe().withSubscriber(UniAssertSubscriber.create());
+//
+//        // Then
+//        subscriber.assertCompleted();
+//
+//        String xml = subscriber.getItem();
+//        assertSnapshot(xml, getClass(), "icb.xml");
+//        assertSendSunat(xml);
+//    }
 }
