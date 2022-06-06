4cbd0572330a6b2565a50a9365f0d93e0fb23933
Carlos Feria <2582866+carlosthe19916@users.noreply.github.com>
2022-06-06 12:33:13 +0200

    Save changes

--- backend/src/main/java/io/github/project/openubl/ublhub/ubl/XMLBuilder.java
+++ backend/src/main/java/io/github/project/openubl/ublhub/ubl/XMLBuilder.java
@@ -1,8 +1,6 @@
 package io.github.project.openubl.ublhub.ubl;
 
-import io.github.project.openubl.ublhub.ubl.content.models.standard.general.BoletaFactura;
-import io.github.project.openubl.ublhub.ubl.enricher.ContentEnricher;
-import io.github.project.openubl.ublhub.ubl.renderer.Renderer;
+
 import io.smallrye.mutiny.Uni;
 
 import javax.enterprise.context.ApplicationScoped;
@@ -11,15 +9,15 @@ import javax.inject.Inject;
 @ApplicationScoped
 public class XMLBuilder {
 
-    @Inject
-    ContentEnricher contentEnricher;
-
-    @Inject
-    Renderer xmlRenderer;
-
-    public Uni<String> enrichAndRenderAsync(BoletaFactura dto) {
-        BoletaFactura data = contentEnricher.enrich(dto);
-        return xmlRenderer.renderAsync(data);
-    }
+//    @Inject
+//    ContentEnricher contentEnricher;
+//
+//    @Inject
+//    Renderer xmlRenderer;
+//
+//    public Uni<String> enrichAndRenderAsync(BoletaFactura dto) {
+//        BoletaFactura data = contentEnricher.enrich(dto);
+//        return xmlRenderer.renderAsync(data);
+//    }
 
 }
