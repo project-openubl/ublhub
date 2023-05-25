9c6e4e10bab8b02ff7fa769bc4f6d9fd63d6cbb9
carlosthe19916 <2582866+carlosthe19916@users.noreply.github.com>
2023-05-25 12:38:14 +0200

    Remove IT tests
    Signed-off-by: carlosthe19916 <2582866+carlosthe19916@users.noreply.github.com>

--- application/src/test/java/io/github/project/openubl/ublhub/security/AuthProjectResourceIT.java
+++
@@ -1,7 +0,0 @@
-package io.github.project.openubl.ublhub.security;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-public class AuthProjectResourceIT extends AuthProjectResourceTest {
-}
